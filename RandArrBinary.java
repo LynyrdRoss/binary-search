e59d285891f2232750b92aa63ae808c31646922f
LynyrdRoss <lyrhen17@gmail.com>
2016-09-27 05:59:11 +0800

    Add files via upload

---
+++ RandArrBinary.java
@@ -0,0 +1,93 @@
+import java.util.*;
+
+public class RandArrBinary {
+
+	int [] arr;
+	int index, size, capacity;
+	int search = 0;
+
+	public RandArrBinary (int size) {
+		capacity = size;
+		arr = new int [capacity];
+	}
+
+	public void sortArr (int [] num) {
+
+		int temp;
+
+		for (int x = 0; x < num.length - 1; x++) {
+			for (int y = 0; y < num.length - 1; y++) {
+				if (num[y] > num[y+1]) {
+					temp = num[y];
+					num[y] = num[y+1];
+					num[y+1] = temp;
+				}
+			}
+		}
+	}
+
+	public int binarySearch (int [] num, int find) {
+		int left, middle, right;
+
+		left = 0;
+		right = num.length - 1;
+		
+		while (right >= left) {
+			middle = (left + right) / 2;
+			if (num[middle] == find) {
+				return 1;
+			}
+			if (num[middle] < find) {
+				left = middle + 1;
+			}
+			if (num[middle] > find) {
+				right = middle - 1;	
+			}
+		}
+
+		return -1;
+	}
+
+	public static void main(String [] args) {
+
+		Scanner in = new Scanner(System.in);
+		Random rand = new Random();
+
+		int userSize, numFind, target;
+
+		System.out.println();
+		System.out.print("Enter desired size of array: ");
+		userSize = in.nextInt();
+		System.out.println();
+
+		RandArrBinary objClass = new RandArrBinary(userSize);
+
+		System.out.print("Enter number to find: ");
+		numFind = in.nextInt();
+		System.out.println();
+
+		System.out.println("Array values: ");
+		for (int i = 0; i < userSize; i++) {
+			objClass.arr[i] = 1+(rand.nextInt(100));
+		}
+
+		objClass.sortArr(objClass.arr);
+
+		for (int i = 0; i < userSize; i++) {
+			System.out.print("[" + objClass.arr[i] + "] ");	
+		}
+		System.out.println();
+
+		target = objClass.binarySearch(objClass.arr, numFind);
+
+		for (int count = 0; count < userSize; count++) {
+			if (objClass.arr[count] == numFind) {
+				System.out.println(numFind + " is located at element: " + (count+1) + ".");
+			}
+		}
+
+		if (target == -1) {
+			System.out.println("Number is not present in the array.");
+		}
+	}
+}
